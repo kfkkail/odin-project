8c3e3a6d84954155a2323571c5a4076603ae6f3c
Kail Keusch <kfkkail@gmail.com>
2016-11-12 12:30:50 -0500

    Optional part of the project added
    random color is chosen, then every time it’s selected again, the same
    color is darkened.

--- sketchpad/js/index.js
+++ sketchpad/js/index.js
@@ -7,6 +7,10 @@ $(document).ready(function(){
 //reset the sketchpad
 function resetSketchpad() {
   var dimensionPrompt = prompt("How many squares? : ", "30");
+  //scrub the input, don't want to crash the page
+  if(dimensionPrompt > 80) {
+    dimensionPrompt = 80;
+  }
   resetContainer(dimensionPrompt);
   $('.square').fadeTo(100,1);
 }
@@ -30,8 +34,35 @@ function resetContainer(dimension) {
 
   //use the hover function to draw!
   $('.square').hover(function(){
-    $(this).fadeTo(500,0.5);
+
+    var backgroundRGB = $(this).css("background-color");
+    console.log(backgroundRGB);
+
+    //Only choose a random color is the background was grey
+    if(backgroundRGB === 'rgb(128, 128, 128)'){
+      var rando = Math.random()*0xFFFFFF<<0;
+      var color = (rando.toString(16));
+      //leading 0's were removed, add them back in!
+      if(color.length < 6) {
+        var diff = 6-color.length;
+        for(i=0; i < diff; i++) {
+          color = "0"+color;
+        }
+      }
+      color = '#'+color;
+      console.log(rando);
+      console.log(color);
+      $(this).css({'background-color':color,'opacity':'0.2'});
+    } else {
+      //otherwise grab the current color and darken it
+      var rgb = backgroundRGB.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+      var newR = Math.floor(rgb[1]*0.25);
+      var newG = Math.floor(rgb[2]*0.5);
+      var newB = Math.floor(rgb[3]*0.75);
+      var darkenColor = 'rgb('+newR+','+newG+','+newB+')'
+      $(this).css({'background-color':darkenColor,'opacity':'0.2'});
+    }
   }, function(){
-    $(this).fadeTo(500,0.6);
+    $(this).fadeTo(500,1);
   });
 }
