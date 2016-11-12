f615069a8230e6fb557521f32e8765faf04040a2
Kail Keusch <kfkkail@gmail.com>
2016-11-11 21:12:40 -0500

    Started grid and added button

--- sketchpad/js/index.js
+++ sketchpad/js/index.js
@@ -1,12 +1,22 @@
 $(document).ready(function(){
-  var dimension = 16;
-  var sketchPadSize = 960;
+  var dimension = 30;
+  var sketchPadSize = 500;
   var squareDimension = sketchPadSize/dimension;
+  $('.container').css({'height':sketchPadSize+'px','width':sketchPadSize+'px'})
 
   for(hCount = 0; hCount < dimension; hCount++) {
     for(wCount = 0; wCount < dimension; wCount++) {
       $('.container').append('<div class=\'#'+hCount+' square\'></div>');
     }
   }
-  $('.square').css({'height':squareDimension+'px','width':squareDimension+'px','background-color':'yellow'});
+  $('.square').css({'height':squareDimension+'px','width':squareDimension+'px','background-color':'grey'});
+  $('.square').hover(function(){
+    $(this).fadeTo(500,0.5);
+  }, function(){
+    $(this).fadeTo(500,1);
+  });
 });
+
+function resetSketchpad() {
+  $('.square').fadeTo(100,1);
+}
