0b41040220ef92a5d68a8b43e003ba63b5aa78e1
Kail Keusch <kfkkail@gmail.com>
2016-11-12 11:03:59 -0500

    basic implementation of the sketchpad

--- sketchpad/js/index.js
+++ sketchpad/js/index.js
@@ -1,22 +1,37 @@
+//document is ready
 $(document).ready(function(){
   var dimension = 30;
+  resetContainer(dimension);
+});
+
+//reset the sketchpad
+function resetSketchpad() {
+  var dimensionPrompt = prompt("How many squares? : ", "30");
+  resetContainer(dimensionPrompt);
+  $('.square').fadeTo(100,1);
+}
+
+//reset the whole container
+function resetContainer(dimension) {
+  //remove all the squares
+  $('.square').remove();
   var sketchPadSize = 500;
   var squareDimension = sketchPadSize/dimension;
+  var numberOfSquares = dimension*dimension;
   $('.container').css({'height':sketchPadSize+'px','width':sketchPadSize+'px'})
 
-  for(hCount = 0; hCount < dimension; hCount++) {
-    for(wCount = 0; wCount < dimension; wCount++) {
-      $('.container').append('<div class=\'#'+hCount+' square\'></div>');
-    }
+  //create all the squares for the sketchpad
+  for(count = 0; count < numberOfSquares; count++) {
+      $('.container').append('<div class=\'square\'></div>');
   }
+
+  //set the css for the squares
   $('.square').css({'height':squareDimension+'px','width':squareDimension+'px','background-color':'grey'});
+
+  //use the hover function to draw!
   $('.square').hover(function(){
     $(this).fadeTo(500,0.5);
   }, function(){
-    $(this).fadeTo(500,1);
+    $(this).fadeTo(500,0.6);
   });
-});
-
-function resetSketchpad() {
-  $('.square').fadeTo(100,1);
 }
