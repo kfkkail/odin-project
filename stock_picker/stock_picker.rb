e1f2398a6b745a7b7ddf593f1c64f2b70bf842e7
Kail <kfkkail@gmail.com>
2016-12-20 18:46:55 +0000

    Refactor Stock_Picker to implement max evaluation in for loop

--- stock_picker/stock_picker.rb
+++ stock_picker/stock_picker.rb
@@ -1,26 +1,22 @@
 def stock_picker(array)
     buy_day = 0
     sell_day = 0
-    diff_matrix = Array.new(array.length*4)
-    buy_days = Array.new(array.length*4)
-    sell_days = Array.new(array.length*4)
-    count = 0
+    profit = 0
     
     for i in 0..(array.length-2)
         buy_value = array[i]
         for j in (i+1)..(array.length-1)
             sell_value = array[j]
-            diff_matrix[count] = sell_value-buy_value
-            buy_days[count] = i
-            sell_days[count] = j
-            count += 1
+            diff = sell_value-buy_value
+            if diff > profit
+                profit = diff
+                buy_day = i
+                sell_day = j
+            end
         end
     end
-    index = diff_matrix.index {|x| x == diff_matrix.max}
-    buy_day = buy_days[index]
-    sell_day = sell_days[index]
     
-    return puts "The best time to buy is day #{buy_day} and the best time to sell is day #{sell_day} with a profit of $#{diff_matrix.max}"
+    return puts "The best time to buy is day #{buy_day} and the best time to sell is day #{sell_day} with a profit of $#{profit}"
 end
 
 stock_picker([17,3,6,9,15,8,6,1,10])
